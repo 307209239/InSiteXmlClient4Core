42c0dc69f9540c7b04deb48a89e61b939230fbaf
ldz <ldz88@outlook.com>
2021-02-06 22:44:18 +0800

    添加ICsiSelectionValue 实现类 CsiSelectionValue

---
+++ Api/CsiSelectionValue.cs
@@ -0,0 +1,31 @@
+﻿using InSiteXmlClient4Core.InterFace;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace InSiteXmlClient4Core.Api
+{
+    internal class CsiSelectionValue : CsiXmlElement, ICsiSelectionValue, ICsiXmlElement
+    {
+        public CsiSelectionValue(ICsiDocument doc, XmlElement domElement)
+          : base(doc, domElement)
+        {
+        }
+
+        public virtual string GetDisplayName()
+        {
+            return this.GetData("__displayName");
+        }
+
+        public virtual string GetValue()
+        {
+            return this.GetData("__value");
+        }
+
+        private string GetData(string tagName)
+        {
+            return (this.FindChildByName(tagName) as CsiXmlElement).GetDomElement().FirstChild.Value;
+        }
+    }
+}
